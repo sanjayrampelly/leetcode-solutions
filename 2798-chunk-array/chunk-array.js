10368fe2a772f9ea7442cc6b571455935469e1b4
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-03 20:29:47 +0530

    Time: 51 ms (33.41%) | Memory: 56.7 MB (41.25%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -3,11 +3,11 @@
  * @param {number} size
  * @return {Array}
  */
-var chunk = function(arr, size) {
-    let res=[]
-    for(let i=0;i<arr.length;i+=size){
-        res.push(arr.slice(i,i+size))
-        
+var chunk = function (arr, size) {
+    let res = []
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size))
+
     }
     return res
 };
