8880ae7d88a80fc73689e5078c1350f6f991f86c
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-04 20:06:32 +0530

    Time: 3 ms (15.67%) | Memory: 19.2 MB (35.50%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.py
+++ 4177-reverse-string-prefix/reverse-string-prefix.py
@@ -1,12 +1,11 @@
 class Solution:
     def reversePrefix(self, s: str, k: int) -> str:
-        stack=[]
-        res=""
+        stack = []
+        res = ""
         for i in range(k):
             stack.append(s[i])
 
-        while(len(stack)):
-            res+=stack.pop()
-        res+=s[k:]
+        while len(stack):
+            res += stack.pop()
+        res += s[k:]
         return res
-        
