4321a891d9b1a1a444b6990ef5cf5fe2d84f822e
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-05 20:49:23 +0530

    Time: 3 ms (12.49%) | Memory: 19.2 MB (93.65%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -1,13 +1,13 @@
 class Solution:
     def sumOfUnique(self, a: List[int]) -> int:
-        d={}
+        d = {}
 
         for e in a:
-            d[e]=d.get(e,0)+1
+            d[e] = d.get(e, 0) + 1
 
-        su=0
-        for k,v in d.items():
-            if d[k]==1:
-                su+=k
-            
-        return su        
+        su = 0
+        for k, v in d.items():
+            if d[k] == 1:
+                su += k
+
+        return su
