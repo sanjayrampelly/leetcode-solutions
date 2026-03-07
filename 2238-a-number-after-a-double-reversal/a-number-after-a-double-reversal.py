b1cdc8e9fc0b12250f8b26a8c2c2349b279490a7
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-07 20:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (50.00%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSameAfterReversals(self, n: int) -> bool:
+        org=n
+        rev=0
+        while(n>0):
+            rem=n%10
+            rev=rev*10+rem
+            n=n//10
+        print(rev)
+        rev1=0
+        while(rev>0):
+            rem=rev%10
+            rev1=rev1*10+rem
+            rev=rev//10
+        print(rev1)
+        return org==rev1
