6cd8a2b19bcefa32f509b848f58210a01fdc4d68
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-12 18:58:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (96.31%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.py
@@ -1,15 +1,12 @@
 class Solution:
     def countDigits(self, n: int) -> int:
-        c=0
-        temp=n
+        c = 0
+        temp = n
 
-        while(n>0):
-            rem=n%10
-            if(temp%rem==0):
-                c+=1
-            n//=10
+        while n > 0:
+            rem = n % 10
+            if temp % rem == 0:
+                c += 1
+            n //= 10
 
         return c
-
-        
-
