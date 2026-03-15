0177eb3e97e5223606a3099dd95373f034f87eb5
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-15 17:19:06 +0530

    Time: 3 ms (67.75%) | Memory: 19.3 MB (54.67%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,10 +1,18 @@
 class Solution:
     def findMissingElements(self, a: List[int]) -> List[int]:
-        a.sort()
-        n = len(a)
-        res = []
-        for i in range(1, len(a)):
-            if a[i] - a[i - 1] != 1:
-                res.extend(range(a[i - 1] + 1, a[i]))
+        # a.sort()
+        # n = len(a)
+        # res = []
+        # for i in range(1, len(a)):
+        #     if a[i] - a[i - 1] != 1:
+        #         res.extend(range(a[i - 1] + 1, a[i]))
 
+        # return res
+        min1=min(a)
+        max1=max(a)
+        s=set(a)
+        res=[]
+        for i in range(min1,max1):
+            if i not in s:
+                res.append(i)
         return res
