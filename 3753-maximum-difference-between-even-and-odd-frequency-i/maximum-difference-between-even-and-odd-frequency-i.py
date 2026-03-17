2b168e3c59fc5d9120b4d8a982c8de06778ad75b
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-17 19:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (89.33%) - LeetSync

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        d={}
+        maxodd=0
+        maxeven=float("Inf")
+        for ch in s:
+            d[ch]=d.get(ch,0)+1
+
+        for i in d.values():
+            if i%2==0:
+                maxeven=min(maxeven,i)
+            else:
+                maxodd=max(maxodd,i)
+
+        return (maxodd-maxeven)
+        
