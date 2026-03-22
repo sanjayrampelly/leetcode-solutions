96220665cf852a92a4611b89ddd09c663d5c911a
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-22 18:52:59 +0530

    Time: 3 ms (44.78%) | Memory: 19.3 MB (49.16%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,7 +1,7 @@
 class Solution:
     def getSneakyNumbers(self, a: List[int]) -> List[int]:
-        s=set()
-        res=[]
+        s = set()
+        res = []
 
         for e in a:
             if e in s:
