5095c8f570dfb00d7eb8e0fbe9fbad63db9e3ccd
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-23 16:44:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.49%) - LeetSync

--- 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.py
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.py
@@ -1,8 +1,8 @@
 class Solution:
     def stableMountains(self, a: List[int], t: int) -> List[int]:
-        res=[]
-        for i in range(len(a)-1):
-            if a[i]>t:
-                res.append(i+1)
+        res = []
+        for i in range(len(a) - 1):
+            if a[i] > t:
+                res.append(i + 1)
 
         return res
