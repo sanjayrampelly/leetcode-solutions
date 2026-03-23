631de786091c6c67cd1104ce4091442ba95d51e8
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-23 19:06:37 +0530

    Time: 61 ms (74.23%) | Memory: 19.3 MB (95.85%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def shuffle(self, a: List[int], n: int) -> List[int]:
-        res=[]
-        p1=a[:n]
-        p2=a[n:]
+        res = []
+        p1 = a[:n]
+        p2 = a[n:]
         for i in range(n):
             res.append(p1[i])
             res.append(p2[i])
