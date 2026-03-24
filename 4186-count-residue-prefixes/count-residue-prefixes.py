d9c273dd989f16b5ec5abf70b988e6c83bbde676
Sanjay <rampellysanjaykumar454@gmail.com>
2026-03-24 16:08:02 +0530

    Time: 2 ms (61.52%) | Memory: 19.2 MB (79.19%) - LeetSync

---
+++ 4186-count-residue-prefixes/count-residue-prefixes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def residuePrefixes(self, s: str) -> int:
+        seen=set()
+        pre=""
+        c=0
+        for ch in s:
+            pre+=ch
+            seen.add(ch)
+            if len(seen)==len(pre)%3:
+                c+=1
+        return c
+        
