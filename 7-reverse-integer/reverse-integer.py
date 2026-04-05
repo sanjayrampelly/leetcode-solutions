c7f2bd8a70ba93136be1b9ef0e9556ad20eec9c2
Sanjay <rampellysanjaykumar454@gmail.com>
2026-04-05 20:04:43 +0530

    Time: 49 ms (38.67%) | Memory: 19 MB (95.71%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -10,9 +10,10 @@ class Solution:
         while temp > 0:
             rem = temp % 10
             rev = rev * 10 + rem
+            if rev < -2**31 or rev > 2**31 - 1:
+                return 0
             temp = temp // 10
 
-        if rev < 2**-31 or rev > 2**31 - 1:
-            return 0
+
 
         return rev * sign
