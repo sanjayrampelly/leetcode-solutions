6db4377aabde3f0ac3dc51f2024206adb4cae334
Sanjay <rampellysanjaykumar454@gmail.com>
2026-05-11 19:47:58 +0530

    Time: 35 ms (19.41%) | Memory: 20.1 MB (72.60%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,17 +1,17 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res=[]
-        for i in range(n+1):
+        res = []
+        for i in range(n + 1):
             res.append(self.rightshift(i))
 
         return res
 
-
-
-    def rightshift(self,n):
-        c=0
-        while(n>0):
-            if(n&1):
-                c+=1
-            n=n>>1
+    def rightshift(self, n):
+        c = 0
+        while n > 0:
+            # if n & 1:
+            #     c += 1
+            # n = n >> 1
+            c += 1
+            n = n & (n - 1)
         return c
